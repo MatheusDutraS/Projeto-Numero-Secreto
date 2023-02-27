0585c0395a0f871cb1d50f96dc70e39610bd04f6
Matheus Dutra <matheus.dutra.souza11@gmail.com>
2023-02-27 20:12:06 -0300

    Comentando tags no HTML, Coletando o que foi dito e inserindo em tags no HTML, comentando o código da página

--- js/reconhecimentoDeVoz.js
+++ js/reconhecimentoDeVoz.js
@@ -1,11 +1,23 @@
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
+const elementoChute = document.getElementById('chute')   //Obtém o elemento chute no HTML
+
 const recognition = new SpeechRecognition();
-recognition.lang = 'pt-Br'
+recognition.lang = 'pt-Br'   //Configuração do idioma para pt-br
 recognition.start()
 
-recognition.addEventListener('result', onSpeak)
+recognition.addEventListener('result', onSpeak)   //Ao falar executa a função onSpeak
 
-function onSpeak(e) {
-    console.log(e)
+function onSpeak(e) {   //e significa evento
+    chute = e.results[0][0].transcript   //Pega o que foi dito e insere no elemento chute
+    exibeChute(chute)
 }
+
+
+function exibeChute(chute) {   //Função que exibe o que foi dito na tela
+    elementoChute.innerHTML = `
+        <div>Você disse: </div>
+        <span class="box">${chute}</span>
+    `
+}
+//Criação de uma div e span no HTML
--- js/sortearNumero.js
+++ js/sortearNumero.js
@@ -1,15 +1,17 @@
-const menorValor = 1;
-const maiorValor = 100;
-const numeroSecreto = gerarNumeroAleatorio()
+const menorValor = 1;   //Definindo o menor valor
+const maiorValor = 100;   //Definindo o maior valor
+const numeroSecreto = gerarNumeroAleatorio()   //Criação de uma constante na qual está é o resultado de uma função
 
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * maiorValor + 1)
+function gerarNumeroAleatorio() {   //Função que gera o número aleatório
+    return parseInt(Math.random() * maiorValor + 1)   //quando a função é chamada retorna um valor aleatório entre 1 e 100
 }
 
 console.log(numeroSecreto)
 
 const elementoMenorValor = document.getElementById('menorValor');
 const elementoMaiorValor = document.getElementById('maiorValor');
+//Obtém as tags span com ids maior valor e menor valor
 
 elementoMenorValor.innerHTML = menorValor
 elementoMaiorValor.innerHTML = maiorValor
+//Insere no HTML os valores definidos como maior e menor
